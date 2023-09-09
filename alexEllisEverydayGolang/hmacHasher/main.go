bfec3f41fd0c60b36d19734f5ffa1603bb4b5b31
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-09-09 19:06:00 +0530

    alexEll:Refactored HMAC Hasher using Pointers

--- alexEllisEverydayGolang/hmacHasher/main.go
+++ alexEllisEverydayGolang/hmacHasher/main.go
@@ -1,3 +1,7 @@
+// Generate HMAC Digests for msg, using a key.
+// Sample command to run:
+//		go run main.go --msg="Hi." --key="1234"
+
 package main
 
 import (
@@ -14,21 +18,28 @@ func main() {
 		srcString string
 		secretKey string
 	)
+
+	parseValidateInputs(&srcString, &secretKey)
+	computeAndPrintHash(&srcString, &secretKey)
+}
+
+func computeAndPrintHash(srcString, secretKey *string) {
+	fmt.Printf("Computing hash for Src string: %q\nSecret key: %q\n", *srcString, *secretKey)
+	digest := signHmac([]byte(*srcString), []byte(*secretKey))
+	fmt.Printf("Digest: %x\n", digest)
+}
+
+func parseValidateInputs(srcString, secretKey *string) {
 	// Parse Inputs
-	flag.StringVar(&srcString, "msg", "", "Src string whose hash is required")
-	flag.StringVar(&secretKey, "key", "", "Non empty Secret key")
+	flag.StringVar(srcString, "msg", "", "Src string whose hash is required")
+	flag.StringVar(secretKey, "key", "", "Non empty Secret key")
 	flag.Parse()
 
 	// Inout Validation
-	if len(strings.TrimSpace(srcString)) == 0 || len(strings.TrimSpace(secretKey)) == 0 {
+	if len(strings.TrimSpace(*srcString)) == 0 || len(strings.TrimSpace(*secretKey)) == 0 {
 		fmt.Println("❌ Src string , or Key is empty or whitesspaced")
 		os.Exit(1)
 	}
-
-	// Compute & Print Hash.
-	fmt.Printf("Computing hash for Src string: %q\nSecret key: %q\n", srcString, secretKey)
-	digest := signHmac([]byte(srcString), []byte(secretKey))
-	fmt.Printf("Digest: %x\n", digest)
 }
 
 func signHmac(msg, key []byte) []byte {
